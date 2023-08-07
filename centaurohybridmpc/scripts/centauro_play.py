8d74377ff79e1f41bf8be309422f4d2491b231a3
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-08-07 19:04:57 +0200

    moved to monotonic timers

--- centaurohybridmpc/scripts/centauro_play.py
+++ centaurohybridmpc/scripts/centauro_play.py
@@ -56,12 +56,12 @@ rt_factor = 1.0
 real_time = 0.0
 sim_time = 0.0
 i = 0
-start_time = time.time()
+start_time = time.monotonic()
 start_time_loop = 0
 
 while env._simulation_app.is_running():
     
-    start_time_loop = time.time()
+    start_time_loop = time.monotonic()
 
     # if (i >= rt_time_reset):
 
@@ -75,7 +75,7 @@ while env._simulation_app.is_running():
 
     obs, rewards, dones, info = env.step(index=i) 
     
-    now = time.time()
+    now = time.monotonic()
     real_time = now - start_time
     sim_time += sim_params["integration_dt"]
     rt_factor = sim_time / real_time
