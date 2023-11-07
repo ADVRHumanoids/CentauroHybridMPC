fc3bc61d66ac76f750314b9ccee291636daf23ea
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2023-11-07 15:19:25 +0100

    wrapping input angles [-2pi, 2pi] to avoid simulation explosion

--- centaurohybridmpc/controllers/centauro_rhc/centaurorhc.py
+++ centaurohybridmpc/controllers/centauro_rhc/centaurorhc.py
@@ -282,7 +282,7 @@ class CentauroRHC(RHController):
 
         return torch.tensor(self._ti.solution['q'][7:, 0], 
                         dtype=self.array_dtype).reshape(1, 
-                                        self.robot_cmds.jnt_cmd.q.shape[1])
+                                        self.robot_cmds.jnt_cmd.q.shape[1]).fmod(2 * torch.pi)
     
     def _get_cmd_jnt_v_from_sol(self):
 
