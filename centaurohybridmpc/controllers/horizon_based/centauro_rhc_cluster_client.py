de9a57dda1a459e09cd99b99f0b2e9e743a59b12
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-07-16 14:27:01 +0200

    fixed xrdf generation files

---
+++ centaurohybridmpc/controllers/horizon_based/centauro_rhc_cluster_client.py
@@ -0,0 +1,71 @@
+from lrhc_control.controllers.rhc.lrhc_cluster_client import LRhcClusterClient
+from lrhc_control.controllers.rhc.hybrid_quad_client import HybridQuadrupedClusterClient
+
+from kyonrlstepping.controllers.horizon_based.kyon_rhc import KyonRhc
+from kyonrlstepping.utils.xrdf_gen import get_xrdf_cmds_horizon
+
+from typing import List
+
+class KyonRHCLusterClient(HybridQuadrupedClusterClient):
+    
+    def __init__(self, 
+            namespace: str, 
+            cluster_size: int,
+            robot_pkg_pref_path: str,
+            set_affinity: bool = False,
+            use_mp_fork: bool = False,
+            isolated_cores_only: bool = False,
+            core_ids_override_list: List[int] = None,
+            verbose: bool = False,
+            debug: bool = False,
+            open_loop: bool = True,
+            with_wheels: bool = False,
+            base_dump_dir: str = "/tmp",
+            timeout_ms: int = 60000,
+            codegen_override: str = ""):
+        
+        self._with_wheels = with_wheels
+
+        super().__init__(namespace = namespace, 
+            robot_pkg_name="kyon",
+            robot_pkg_pref_path=robot_pkg_pref_path,
+            cluster_size=cluster_size,
+            isolated_cores_only = isolated_cores_only,
+            set_affinity = set_affinity,
+            use_mp_fork = use_mp_fork,
+            core_ids_override_list = core_ids_override_list,
+            verbose = verbose,
+            debug = debug,
+            open_loop=open_loop,
+            base_dump_dir=base_dump_dir,
+            timeout_ms=timeout_ms,
+            codegen_override=codegen_override)
+
+    def _xrdf_cmds(self):
+        cmds = get_xrdf_cmds_horizon(robot_pkg_name = self.robot_pkg_name,
+                            robot_pkg_pref_path= self.robot_pkg_pref_path,
+                            with_wheels = self._with_wheels)
+        return cmds
+
+    def _generate_controller(self,
+        idx: int):
+        
+        codegen_dir=self._process_codegen_dir(idx=idx)
+
+        controller = KyonRhc(
+                urdf_path=self._urdf_path, 
+                srdf_path=self._srdf_path,
+                robot_name=self._namespace,
+                codegen_dir=codegen_dir,
+                with_wheels=self._with_wheels,
+                n_nodes=31, 
+                dt=0.03,
+                injection_node=5,
+                max_solver_iter = 1,
+                open_loop = self._open_loop,
+                verbose = self._verbose, 
+                debug = self._debug,
+                refs_in_hor_frame=True,
+                timeout_ms=self._timeout_ms)
+        
+        return controller 
---
+++ centaurohybridmpc/utils/centauro_urdf_gen.py
@@ -0,0 +1,48 @@
+from rhcviz.utils.xrdf_gen import UrdfGenerator
+
+class CentauroUrdfGen(UrdfGenerator):
+
+    def __init__(self, 
+            robotname: str,
+            descr_path: str,
+            big_wheels: bool = True,
+            name: str = "CentauroUrdfRHCViz"):
+        
+        super().__init__(
+            robotname = robotname,
+            descr_path = descr_path,
+            name = name)
+
+        self._big_wheels = big_wheels
+
+        self.generate_urdf() # actually generated urdf
+
+    def _xrdf_cmds(self):
+        
+        # implements parent method 
+
+        cmds = {} 
+        
+        cmds[self.robotname] = self._get_xrdf_cmds_centauro(root=self.descr_path)
+        
+        return cmds
+    
+    def _get_xrdf_cmds_centauro(self,
+                root: str):
+        
+        cmds = []
+        
+        if self._big_wheels:
+            cmds.append("big_wheel:=true")
+        else:
+            cmds.append("big_wheel:=false")
+
+        cmds.append("legs:=true")
+        cmds.append("upper_body:=true")
+        cmds.append("velodyne:=false")
+        cmds.append("realsense:=false")
+        cmds.append("floating_joint:=false")
+        cmds.append("use_abs_mesh_paths:=true")
+        cmds.append("use_local_filesys_for_meshes:=true")
+
+        return cmds
