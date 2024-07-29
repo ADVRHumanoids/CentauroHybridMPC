b68e99fe011aa990a188df7929d3c76c29a34707
AndrePatri <andreapatrizi1b6e6@gmail.com>
2024-07-29 13:28:12 +0200

    Signed-off-by: AndrePatri <andreapatrizi1b6e6@gmail.com>

--- centaurohybridmpc/controllers/horizon_based/centauro_rhc.py
+++ centaurohybridmpc/controllers/horizon_based/centauro_rhc.py
@@ -392,7 +392,7 @@ class CentauroRhc(HybridQuadRhc):
         q_root = self.robot_state.root_state.get(data_type="q", robot_idxs=self.controller_index).reshape(-1, 1)
         p = self.robot_state.root_state.get(data_type="p", robot_idxs=self.controller_index).reshape(-1, 1)
         if not close_all: # use internal MPC for the base
-            p[0:3,:]=self._ti.solution['q'][0:3, 1] # base pos is open loop
+            p[0:3,:]=self._ti.solution['q'][0:3, 1:2] # base pos is open loop
         v_root = self.robot_state.root_state.get(data_type="v", robot_idxs=self.controller_index).reshape(-1, 1)
         omega = self.robot_state.root_state.get(data_type="omega", robot_idxs=self.controller_index).reshape(-1, 1)
         
