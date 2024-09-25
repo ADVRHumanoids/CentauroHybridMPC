c63f2d5dc81e37b25c0339fe9c2755043459c2da
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-09-25 12:34:34 +0200

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- centaurohybridmpc/controllers/horizon_based/centauro_rhc.py
+++ centaurohybridmpc/controllers/horizon_based/centauro_rhc.py
@@ -64,6 +64,7 @@ class CentauroRhc(HybridQuadRhc):
         else:
             self._fail_idx_thresh=self._fail_idx_thresh_close_loop
         
+    def _set_rhc_pred_idx(self):
         self._pred_node_idx=round((self._n_nodes-1)*2/3)
 
     def _init_rhc_task_cmds(self):
