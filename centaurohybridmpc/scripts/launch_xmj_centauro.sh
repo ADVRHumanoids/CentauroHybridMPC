37aff423025ac5b341c810d83174571a31a17b0c
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-11-13 12:13:09 +0100

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- centaurohybridmpc/scripts/launch_xmj_centauro.sh
+++ centaurohybridmpc/scripts/launch_xmj_centauro.sh
@@ -6,8 +6,8 @@ source /opt/xbot/setup.sh
 source /root/ibrido_ws/setup.bash
 
 python /root/ibrido_ws/src/xbot2_mujoco/tests/PyXBotMjSim/launch_simulator.py --urdf_path /tmp/RtDeploymentEnv/centauro_big_wheels_no_yaw.urdf \
-    --simopt_path /root/ibrido_ws/src/xbot2_mujoco/tests/files/centauro/sim_opt.xml \
-    --world_path /root/ibrido_ws/src/xbot2_mujoco/tests/files/centauro/world.xml \
-    --sites_path /root/ibrido_ws/src/xbot2_mujoco/tests/files/centauro/sites.xml \
-    --xbot_config_path /root/ibrido_ws/src/xbot2_mujoco/tests/files/centauro/xbot2_basic.yaml \
+    --simopt_path /root/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/xmj_env_files/sim_opt.xml \
+    --world_path /root/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/xmj_env_files/world.xml \
+    --sites_path /root/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/xmj_env_files/sites.xml \
+    --xbot_config_path /root/ibrido_ws/src/CentauroHybridMPC/centaurohybridmpc/config/xmj_env_files/xbot2_basic.yaml \
     --pub_rostime --blink_name base_link
